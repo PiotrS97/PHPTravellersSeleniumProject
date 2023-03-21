c18d5cd22d9c012e384e202c49c632febc1841df
PiotrS97 <piotr.szatkowski97@gmail.com>
2023-03-21 19:16:58 +0100

    Add ResultsPage and SignUpPage Cleaning up code

---
+++ src/test/java/pl/seleniumdemo/pages/SignUpPage.java
@@ -0,0 +1,57 @@
+package pl.seleniumdemo.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SignUpPage {
+    public SignUpPage(WebDriver driver){
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(name = "firstname")
+    private WebElement firstNameInput;
+    @FindBy(name = "lastname")
+    private WebElement lastNameInput;
+    @FindBy(name = "phone")
+    private WebElement phoneInput;
+    @FindBy(name = "email")
+    private WebElement emailInput;
+    @FindBy(name = "password")
+    private WebElement passwordInput;
+    @FindBy(name = "confirmpassword")
+    private WebElement confirmPasswordInput;
+    @FindBy(xpath = "//button[text()=' Sign Up']")
+    private WebElement signUpButton;
+
+    public void setFirstName(String firstName){
+        firstNameInput.sendKeys(firstName);
+    }
+
+    public void setLastName(String lastName){
+        lastNameInput.sendKeys(lastName);
+    }
+
+    public void setPhone(String phone){
+        phoneInput.sendKeys(phone);
+    }
+
+    public void setEmail(){
+        emailInput.sendKeys("abc"+ System.currentTimeMillis() +"@cbz.pl");
+    }
+
+    public void setPassword(String password){
+        passwordInput.sendKeys(password);
+    }
+
+    public void setPasswordConfirmation(String password){
+        confirmPasswordInput.sendKeys(password);
+    }
+
+    public void performSignUp(){
+        signUpButton.click();
+    }
+
+}
